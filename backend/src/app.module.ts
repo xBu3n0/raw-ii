a9e2055d69987264d6c5e20cd713bebe3b0923d8
Matheus Bueno <matheusbartkev@gmail.com>
2026-02-19 20:17:28 -0300

    feat(metrics + prometeus + grafana)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { PrometheusModule } from "@willsoto/nestjs-prometheus";
 
 @Module({
-    imports: [],
+    imports: [
+        PrometheusModule.register(),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,4 +5,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(80);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
