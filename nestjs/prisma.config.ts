b0d0edabcb7a7e7da146c7d9d15e9c8235712b72
Matheus Bueno <matheusbartkev@gmail.com>
2026-03-01 20:50:05 -0300

    fix(prisma)

--- nestjs/prisma.config.ts
+++ nestjs/prisma.config.ts
@@ -4,7 +4,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "src/prisma/schema.prisma",
+  schema: "src/prisma/schema",
   migrations: {
     path: "src/prisma/migrations",
   },
